2e7f7d4365603687e58095b8dfb5968ca9d93bbf
Neo Hsu <locu1214@gmail.com>
2020-08-11 16:48:51 +0800

    非同步處理的 callback function 如果要存取到上一層的變數時，必須使用 arrow function 或用 that

--- Lab1_Node_NPM_JS/lab46_async_property.js
+++ Lab1_Node_NPM_JS/lab46_async_property.js
@@ -3,22 +3,26 @@
  * fs.open
  * fs.close
  * 都要處理 error 與 handler 喔！！
+ * 非同步處理的 callback function 如果要存取到上一層的變數時，必須使用 arrow function 或用 that
  */
 const fs = require('fs')
 function FileObject() {
     this.filename = ''
     this.file_exists = function (callback) {
+        //const that = this
         console.log("What is this?" + this, this.filename)
         console.log("about to open:" + this.filename)
+        
         // fs.open 為非同步
-        fs.open(this.filename, 'r', function (error, handler) {
+        //fs.open(this.filename, 'r', function (error, handler) {
+        fs.open(this.filename, 'r', (error, handler) => {
             console.log("What is this?" + this, this.filename)
             if (error) {
-                console.log("can not open: " + this.filename) // 這邊要用this存取filename變數，則一定要用 arrow function！
+                console.log("can not open: " + this.filename) // 這邊要用this存取filename變數，則一定要用 arrow function 或用 that
                 callback(error)
                 return
             }
-            console.log("the file we process is " + this.filename)
+            console.log("the file we process is " + that.filename)
             console.log("now we can process file with handler" + handler)
 
             // fs.close 為非同步
@@ -30,7 +34,7 @@ function FileObject() {
 }
 
 let fo1 = new FileObject()
-fo1.filename = "../README.md"
+fo1.filename = "../README.mdx"
 fo1.file_exists((error, result) => {
     if (error) {
         console.log("error open file:" + error)
