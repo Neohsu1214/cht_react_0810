99682acce1ea6af59f60ac260e5275815d57937c
Neo Hsu <locu1214@gmail.com>
2020-08-17 10:32:57 +0800

    針對 buffer 做切割: slice

---
+++ Lab1_Node_NPM_JS/lab57_buffer_slice.js
@@ -0,0 +1,16 @@
+/**
+ * 針對 buffer 做切割: slice
+ */
+
+const buffer1 = Buffer.allocUnsafe(26)
+
+for (let i = 0; i < 26; i++) {
+    buffer1[i] = i + 97
+}
+
+const buffer2 = buffer1.slice(0, 3)
+console.log('buffer2 content: ' + buffer2.toString()) // abc
+
+const buffer3 = new Buffer.from('www.uuu.com.tw')
+const buffer4 = buffer3.slice(0, 9)
+console.log('buffer4 content: ' + buffer4.toString()) // www.uuu.c
