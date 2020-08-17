1989fb68db235bf776b9e203b0a758c726bf287e
Neo Hsu <locu1214@gmail.com>
2020-08-17 10:34:10 +0800

    針對 buffer 做切割: slice

--- Lab1_Node_NPM_JS/lab57_buffer_slice.js
+++ Lab1_Node_NPM_JS/lab57_buffer_slice.js
@@ -8,7 +8,7 @@ for (let i = 0; i < 26; i++) {
     buffer1[i] = i + 97
 }
 
-const buffer2 = buffer1.slice(0, 3)
+const buffer2 = buffer1.slice(0, 3) // 從 0 開始數 3個bytes
 console.log('buffer2 content: ' + buffer2.toString()) // abc
 
 const buffer3 = new Buffer.from('www.uuu.com.tw')
