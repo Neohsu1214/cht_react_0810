98ea1a4754a9dee91927cff3af4946aadfb93c1f
Neo Hsu <locu1214@gmail.com>
2020-08-17 14:35:30 +0800

    定義一個可 export 的JS物件

---
+++ Lab2_Babel_Modern_JS_Syntax/src/lab3_course.js
@@ -0,0 +1,10 @@
+/**
+ * 定義一個可 export 的JS物件
+ */
+ const course = {
+     name: "react with spring boot",
+     duration: 56,
+     instructor: "Mark",
+     period: ['Mon', 'Tue']
+ }
+ export default course
