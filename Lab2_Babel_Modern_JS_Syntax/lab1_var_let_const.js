53dba9c4c55a9b0bc141a9b8e03cab6f58597386
Neo Hsu <locu1214@gmail.com>
2020-08-17 13:47:54 +0800

    透過以下指令將結果輸出到檔案中 npx babel lab1_var_let_const.js --out-file out/lab1_var_let_const.js

--- Lab2_Babel_Modern_JS_Syntax/lab1_var_let_const.js
+++ Lab2_Babel_Modern_JS_Syntax/lab1_var_let_const.js
@@ -1,9 +1,15 @@
 /**
  * 介紹Babel如何將不支援的ES語法轉換成瀏覽器可執行的JS版本
- * npx babel lab1_var_let_const.js
+ *  npx babel lab1_var_let_const.js
  * 請注意，要給定 .babelrc 檔，babel才會進行轉換
  * 在執行一次轉譯，會發現產出的JS內容不一樣囉！
  * 是很標準的JS語法了！！！
+ * 可透過以下指令將結果輸出到檔案中
+ *  npx babel lab1_var_let_const.js --out-file out/lab1_var_let_const.js
+ * 再執行 node out/lab1_var_let_const.js
+ * 會發現輸出的結果與
+ *       node lab1_var_let_const.js
+ * 是相同的！（當然啦！不然babel要打屁股了)
  */
 
 let x = 5
