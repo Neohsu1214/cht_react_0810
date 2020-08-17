060a818e9f8fa9d506d2260d932e896dba752e06
Neo Hsu <locu1214@gmail.com>
2020-08-17 13:44:36 +0800

    * 介紹Babel如何將不支援的ES語法轉換成瀏覽器可執行的JS版本  * npx babel lab1_var_let_const.js  * 請注意，要給定 .babelrc 檔，babel才會進行轉換  * 在執行一次轉譯，會發現產出的JS內容不一樣囉！  * 是很標準的JS語法了！！！

--- Lab2_Babel_Modern_JS_Syntax/lab1_var_let_const.js
+++ Lab2_Babel_Modern_JS_Syntax/lab1_var_let_const.js
@@ -1,6 +1,9 @@
 /**
  * 介紹Babel如何將不支援的ES語法轉換成瀏覽器可執行的JS版本
  * npx babel lab1_var_let_const.js
+ * 請注意，要給定 .babelrc 檔，babel才會進行轉換
+ * 在執行一次轉譯，會發現產出的JS內容不一樣囉！
+ * 是很標準的JS語法了！！！
  */
 
 let x = 5
@@ -12,3 +15,17 @@ const y = `hello world, actually x=${x}`
 console.log(y)
 const z = `hihi, x=${x}`
 console.log(z)
+
+/* 轉譯後的內容如下
+
+"use strict"; // 不允許使用 global 變數
+
+var x = 5;
+console.log(x);
+x += 1;
+console.log(x);
+var y = "hello world, actually x=".concat(x);
+console.log(y);
+var z = "hihi, x=".concat(x);
+console.log(z);
+*/
