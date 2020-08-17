bb87f3473d1f78a4fbc63de116ad3083424b073f
Neo Hsu <locu1214@gmail.com>
2020-08-17 14:35:21 +0800

    * 定義多個 export 物件於同一個 js 檔案中  * 定義一個可 export 的 function  * 定義一個可 export 的 const參數

---
+++ Lab2_Babel_Modern_JS_Syntax/src/lab3_common_util.js
@@ -0,0 +1,9 @@
+/**
+ * 定義多個 export 物件於同一個 js 檔案中
+ * 定義一個可 export 的 function
+ * 定義一個可 export 的 const參數
+ */
+export const default_header = () => {
+    console.log("___This is a header___")
+}
+export const MAX_SIZE = 50
