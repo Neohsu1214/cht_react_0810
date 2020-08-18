79b2ddaacd03478d7fa1f000955b5440fc0b9ff0
Neo Hsu <locu1214@gmail.com>
2020-08-18 11:17:35 +0800

    可透過 props.children 存取 父comp tag 間的內容（類似Vue的slot）

--- lab3_react_start/src/components/Pet.js
+++ lab3_react_start/src/components/Pet.js
@@ -1,16 +1,20 @@
 /**
  * 說明如何透過 props 傳遞資訊給 Class Based Component
  * 不同於 Functional Based Component，要用 this.props
+ * 可透過 props.children 存取 父component tag 間的內容（類似Vue的slot）
  */
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Pet extends Component {
-    render() {
-        return (
-            <div>
-            I am a {this.props.species}, named {this.props.name}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>
+          I am a {this.props.species}, named {this.props.name}
+        </p>
+        <p>I can {this.props.children}</p>
+      </div>
+    );
+  }
 }
-export default Pet
+export default Pet;
