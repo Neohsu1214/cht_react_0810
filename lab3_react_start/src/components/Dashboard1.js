c70fb25ee27fdfdda2614c6603f8cbac108c1c30
Neo Hsu <locu1214@gmail.com>
2020-08-18 10:24:32 +0800

    * 可透過輸入『rcc』後自動產出 Class Based Component 的 Template  * react class component

---
+++ lab3_react_start/src/components/Dashboard1.js
@@ -0,0 +1,26 @@
+/**
+ * 可透過輸入『rcc』後自動產出 Class Based Component 的 Template
+ * react class component
+import React, { Component } from 'react'
+
+export default class Dashboard1 extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+ */
+import React, { Component } from "react";
+
+export default class Dashboard1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi, I am Dashboard1</h1>
+      </div>
+    );
+  }
+}
