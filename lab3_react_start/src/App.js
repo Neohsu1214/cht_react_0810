9703e00e2f484590ee86804af31e4445a1292f70
Neo Hsu <locu1214@gmail.com>
2020-08-18 14:10:30 +0800

    使用state儲存資料，並於JSX中存取，並透過onClick事件觸發更新state內容

--- lab3_react_start/src/App.js
+++ lab3_react_start/src/App.js
@@ -9,26 +9,36 @@ import Counter from './components/Counter';
 
 //function App() {
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Mark", age: 43 },
+      { name: "James", age: 38 },
+      { name: "Tim", age: 33 },
+      { name: "Mary", age: 28 },
+      { name: "abby", age: 34 },
+      { name: "Kevin", age: 50 }
+
+    ]
+  }
+
+  changeNameHandler = () => {
+    console.log("change button clicked")    
+    this.setState({
+      persons: [
+        {name: "X man", age: 45},
+        {name: "Captain America", age: 35},
+        {name: "Iron man", age: 42},
+        {name: "Thor", age: 200},
+        {name: "Groot", age: 5},
+        {name: "Hawk", age: 40}
+      ]
+    })
+  }
+
   render() {
     // return 的內容其實是 JSX(JavaScriptXML) 而不是 HTML，需要經過React轉換輸出成 HTML+JS+CSS
     // return 的內容只能有一個 root <div>
     return (
-      // <div className="App">
-      //   <h1>Hello World</h1>
-      //   <h2>React Programming</h2>
-      //   <h3>HiHi 尼小歐</h3>
-      // </div>
-
-      // 改用 React.createElement 來達到同樣的輸出結果
-      // React.createElement(
-      //   "div",
-      //   { className: "App" }, // 第二個參數是自定義組態object
-      //   // 第三個以後的參數必須都是透過 React.createElememt產生的 Elements
-      //   React.createElement("h1", null, "Hello World"),
-      //   React.createElement("h2", null, "React Programming"),
-      //   React.createElement("h3", null, "HiHi 尼小歐")
-      // )
-
       // 改用 import 外部 component！直接在 div 內輸入 <component名稱> IDE外掛會自動幫你import進來
       // JSX tag名稱務必要與component名稱大小寫一致（這點跟Vue很不一樣）
       // JSX tag中可加入 attribute 來傳遞資料給 Components
@@ -36,17 +46,18 @@ class App extends Component {
         <Counter step="2"></Counter>
         <Dashboard1></Dashboard1>
         <Dashboard2></Dashboard2>
-        <Person name="Mark" age="43"></Person>
+        <button onClick={this.changeNameHandler}>Change</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Pet name="King" species="cat">
           <p style={{ color: "green" }}>喵</p>
         </Pet>
-        <Person name="James" age="38"></Person>
-        <Person name="Tim" age="33">
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
           Team Leader
         </Person>
-        <Person name="Mary" age="28"></Person>
-        <Person name="Abby" age="34"></Person>
-        <Person name="Kevin" age="50"></Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}></Person>
+        <Person name={this.state.persons[4].name} age={this.state.persons[4].age}></Person>
+        <Person name={this.state.persons[5].name} age={this.state.persons[5].age}></Person>
         <Pet></Pet>
       </div>
     );
