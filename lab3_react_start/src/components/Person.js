bd65d375fc2360ddc353667129a5eef39824856c
Neo Hsu <locu1214@gmail.com>
2020-08-18 14:35:22 +0800

    React Hook，會用新的state覆蓋掉舊的state值，所以如果有要保留的要自行實作

--- lab3_react_start/src/components/Person.js
+++ lab3_react_start/src/components/Person.js
@@ -21,16 +21,23 @@ import React, {useState} from "react";
 // 可透過 props.children 存取 父component tag 間的內容（類似Vue的slot）
 const Person = (props) => {
   // 讓 functional component 也可以用 state (理論上是不行的，所以要宣告成 const)
-  const [stringState, changeString] = useState({publishier: "Marvel Universe"})
+  const [stateString, changeString] = useState({
+    publishier: "Marvel Universe",
+    studio: "Disney"
+  })
   // 雖然 state 宣告成 const，但還是想異動內容！就要靠callback function的指定！
   const switchVendorHandler = () => {
-    changeString({publishier: "DC Universe"})
+    // 注意：此行為叫 React Hook，會用新的state覆蓋掉舊的state值，所以如果有要保留的要自行實作 ex: stateString.studio
+    changeString({publishier: "DC Universe", studio: stateString.studio})
   }
-
+  const showStatusHandler = () => {
+    console.log(stateString)
+  }
   return (
     <div>
-    <button onClick={switchVendorHandler}>Change</button>
-    <h1>{stringState.publishier}</h1>
+    <button onClick={switchVendorHandler}>Change!</button>
+    <button onClick={showStatusHandler}>Show!</button>
+    <h1>{stateString.publishier}</h1>
       <p>I am {props.name}</p>
       <p>I am {props.age} years old</p>
       <p>I am the {Math.floor(Math.random() * 5)}th groot</p>
