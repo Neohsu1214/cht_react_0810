5c37e7238c50defa49f3920acfc99cd748e92283
Neo Hsu <locu1214@gmail.com>
2020-08-18 10:25:43 +0800

    改用 import 外部 component！直接在 div 內輸入 <component名稱> IDE外掛會自動幫你import進來

--- lab3_react_start/src/App.js
+++ lab3_react_start/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 //import logo from './logo.svg';
 import "./App.css";
+import Dashboard1 from "./components/Dashboard1";
+import Dashboard2 from "./components/Dashboard2";
+import Person from "./components/Person";
 
 function App() {
   // return 的內容其實是 JSX(JavaScriptXML) 而不是 HTML，需要經過React轉換輸出成 HTML+JS+CSS
@@ -13,14 +16,22 @@ function App() {
     // </div>
 
     // 改用 React.createElement 來達到同樣的輸出結果
-    React.createElement(
-      "div",
-      { className: "App" }, // 第二個參數是自定義組態object
-      // 第三個以後的參數必須都是透過 React.createElememt產生的 Elements
-      React.createElement("h1", null, "Hello World"),
-      React.createElement("h2", null, "React Programming"),
-      React.createElement("h3", null, "HiHi 尼小歐")
-    )
+    // React.createElement(
+    //   "div",
+    //   { className: "App" }, // 第二個參數是自定義組態object
+    //   // 第三個以後的參數必須都是透過 React.createElememt產生的 Elements
+    //   React.createElement("h1", null, "Hello World"),
+    //   React.createElement("h2", null, "React Programming"),
+    //   React.createElement("h3", null, "HiHi 尼小歐")
+    // )
+
+    // 改用 import 外部 component！直接在 div 內輸入 <component名稱> IDE外掛會自動幫你import進來
+    <div className="App">
+      <Dashboard1></Dashboard1>
+      <Person></Person>
+      <Dashboard2></Dashboard2>
+      <Person></Person>
+    </div>
   );
 }
 
