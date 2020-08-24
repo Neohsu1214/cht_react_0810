fdc5888beb35b0469ada0526c24808b27df49b2c
Neo Hsu <locu1214@gmail.com>
2020-08-24 11:09:54 +0800

    Project template from start.spring.io

---
+++ Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/Backend1GradleApplication.java
@@ -0,0 +1,13 @@
+package com.chtti.fullstack.memo.Backend1_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend1GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend1GradleApplication.class, args);
+	}
+
+}
---
+++ Backend1_gradle/src/test/java/com/chtti/fullstack/memo/Backend1_gradle/Backend1GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chtti.fullstack.memo.Backend1_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend1GradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
