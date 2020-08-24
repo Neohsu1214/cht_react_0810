9f23940615178035a8a1d85e03fd47f187601da2
Neo Hsu <locu1214@gmail.com>
2020-08-24 11:36:46 +0800

    Initial Springboot from start.spring.io

---
+++ Backend1_maven/src/main/java/com/chtti/fullstack/memo/Backend1_maven/Backend1MavenApplication.java
@@ -0,0 +1,13 @@
+package com.chtti.fullstack.memo.Backend1_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend1MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend1MavenApplication.class, args);
+	}
+
+}
---
+++ Backend1_maven/src/test/java/com/chtti/fullstack/memo/Backend1_maven/Backend1MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chtti.fullstack.memo.Backend1_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend1MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
