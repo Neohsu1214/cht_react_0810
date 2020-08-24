0dffe54394031a8a73182763e8b4ed17b89541b4
Neo Hsu <locu1214@gmail.com>
2020-08-24 16:47:59 +0800

    透過在 Controller 中加上 @Valid 來驗證 JPA Entity 的限制 ex: NotBlank 並提供 message 給前端

--- Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/controller/ProjectController.java
+++ Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/controller/ProjectController.java
@@ -2,13 +2,19 @@ package com.chtti.fullstack.memo.Backend1_gradle.controller;
 
 import com.chtti.fullstack.memo.Backend1_gradle.model.Project;
 import com.chtti.fullstack.memo.Backend1_gradle.service.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -16,8 +22,15 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProjectController.class);
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> // 表示可回傳任何型態的 ResponseEntity
+    createNewProject(@Valid @RequestBody Project project
+            , BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         // 送一個 project 進來處理
         Project project1 = projectService.saveOrUpdateProject(project);
         // 處理完後再傳一個 project 回去（難怪TAIS回傳一堆資料還包含我傳進去的，看得好煩，但既然是通則，就接受吧！）
--- Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/model/Project.java
+++ Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/model/Project.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 /**
@@ -27,8 +28,11 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "project name is required!")
     private String projectName;
+    @NotBlank(message = "project identifier is required!")
     private String projectIdentifier;
+    @NotBlank(message = "description is required!")
     private String description;
     @JsonFormat(pattern="yyyy-MM-dd") // 標注日期格式
     private Date startDate;
