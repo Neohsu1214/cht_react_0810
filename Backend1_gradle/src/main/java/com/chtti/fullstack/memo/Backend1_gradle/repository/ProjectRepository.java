18297ad11d073c837e4097a3414c045f20f44b9f
Neo Hsu <locu1214@gmail.com>
2020-08-24 14:28:18 +0800

    * @Repository  * 建立一個 約定俗成名稱 的 CRUD Repository Interface  * 記得後面要 extends CrudRepository<EntityClass名稱, Entity的Id>

--- Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/repository/ProjectRepository.java
+++ Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/repository/ProjectRepository.java
@@ -4,6 +4,11 @@ import com.chtti.fullstack.memo.Backend1_gradle.model.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @Repository
+ * 建立一個 約定俗成名稱 的 CRUD Repository Interface
+ * 記得後面要 extends CrudRepository<EntityClass名稱, Entity的Id>
+ */
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
