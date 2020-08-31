1771e1ffdfa0100eff013dec2e6ad9680d0be721
Neo Hsu <locu1214@gmail.com>
2020-08-31 16:49:07 +0800

    宣告此 Application 啟用 cache

--- Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/Backend1GradleApplication.java
+++ Backend1_gradle/src/main/java/com/chtti/fullstack/memo/Backend1_gradle/Backend1GradleApplication.java
@@ -2,7 +2,9 @@ package com.chtti.fullstack.memo.Backend1_gradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching // 啟用 cache 降低 DB 存取次數 (要先設定 cache 相關參數才能使用喔！)
 @SpringBootApplication
 public class Backend1GradleApplication {
 
