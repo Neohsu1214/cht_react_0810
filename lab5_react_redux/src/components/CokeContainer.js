93e6c46f4530ee76a1ad213667a8fcd56838092f
Neo Hsu <locu1214@gmail.com>
2020-09-01 16:35:34 +0800

    透過react-redux connect將State, Dispatch 與 Props 關聯起來

--- lab5_react_redux/src/components/CokeContainer.js
+++ lab5_react_redux/src/components/CokeContainer.js
@@ -2,14 +2,28 @@
  * 建立一個 Functional Component (short: rfce)
  */
 import React from 'react'
+import { connect } from 'react-redux' // 同樣是 react-redux 但不同於App.js 的Provider 這邊是 connect
+import { buyCoke } from '../redux/coke/CokeAction'
 
-function CokeContainer() {
+
+function CokeContainer(props) {
     return (
         <div>
-            <h3># of Coke</h3>
-            <button>Buy Coke</button>
+            <h3># of Coke={props.numOfCokes}</h3>
+            <button onClick={props.buyCoke}>Buy Coke</button>
         </div> 
     )
 }
+const mapStateToProps = (state) => {
+    return {
+        numOfCokes: state.numOfCokes
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCoke: () => dispatch(buyCoke())
+    }
+}
 
-export default CokeContainer
+// 透過 connect 將 State, Dispatch 與 Props 關聯起來
+export default connect(mapStateToProps, mapDispatchToProps)(CokeContainer)
