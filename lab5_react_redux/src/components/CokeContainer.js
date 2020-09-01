d423539f4b81fe7c25033c3e546c1f22dae0c9a5
Neo Hsu <locu1214@gmail.com>
2020-09-01 15:57:44 +0800

    建立一個 CokeContainer component並顯示於App.js

---
+++ lab5_react_redux/src/components/CokeContainer.js
@@ -0,0 +1,15 @@
+/**
+ * 建立一個 Functional Component (short: rfce)
+ */
+import React from 'react'
+
+function CokeContainer() {
+    return (
+        <div>
+            <h3># of Coke</h3>
+            <button>Buy Coke</button>
+        </div> 
+    )
+}
+
+export default CokeContainer
